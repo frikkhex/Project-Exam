451c916a9b72c918cc39888ad133e294d7f3d7d5
frikkhex <frikkhex@gmail.com>
2021-09-18 21:58:14 +0200

    working javascript

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
-const upcomingLaunchesUrl = "https://api.spacexdata.com/v4/launches/upcoming";
-const upcomingLaunchInput = document.querySelector(".mission-name");
-console.log(upcomingLaunchInput.innerHTML);
+const upcomingLauncUrl = "https://api.spacexdata.com/v4/launches/upcoming/";
+const previousLauncUrl = "https://api.spacexdata.com/v4/launches/past/";
+const upcomingMissionName = document.querySelector(".upcoming");
+const previousMissionName = document.querySelector(".previous");
 
 const fetchData = async () => {
   try {
-    const response = await fetch(upcomingLaunchesUrl);
-    const json = await response.json();
-    const data = json;
+    // Finding the first upcoming launch event
+    const response = await fetch(upcomingLauncUrl);
+    const upcomingLaunchData = await response.json();
+    const dateToday = new Date().toISOString();
+    let prevLaunchDate = [];
+    let upcomingLaunchDate = [];
 
-    const upcomingLaunch = data[0].name;
-    const upcomingLaunchCountdown = data[0].date_utc;
-    console.log(upcomingLaunchCountdown.);
+    // Looping through upcoming events
+    for (let i = 0; i < upcomingLaunchData.length; i++) {
+      const launchDate = upcomingLaunchData[i].date_utc;
+      const launchName = upcomingLaunchData[i].name;
 
-    upcomingLaunchInput.innerHTML = upcomingLaunch;
+      if (launchDate < dateToday) {
+        prevLaunchDate.push(launchName);
+      } else {
+        upcomingLaunchDate.push(launchName);
+      }
+    }
 
-    console.log(upcomingLaunch);
+    const closestDate = upcomingLaunchDate[0];
+    upcomingMissionName.innerHTML = closestDate;
+
+    // Finding the latest launch registered
+    const responsePrev = await fetch(previousLauncUrl);
+    const previousLaunchData = await responsePrev.json();
+    const latestPreviousLaunch = previousLaunchData.reverse()[0];
+    previousMissionName.innerHTML = latestPreviousLaunch.name;
   } catch (err) {
     console.log(err);
   }
